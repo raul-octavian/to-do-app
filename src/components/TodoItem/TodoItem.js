9364bb76aa8e6740466f7ee502c67c92289b050a
raul-octavian <alburaul@gmail.com>
2022-04-14 12:22:56 +0200

    crate the edit functions, sets the button states to Start, pause, and complete

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,7 +2,7 @@
 // import PropTypes from 'prop-types'
 import './TodoItem.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faPlay, faPause, faCheck, faEdit } from '@fortawesome/free-solid-svg-icons'
 import { useOutletContext } from 'react-router-dom'
 import { uri } from '../../Utils/Uri'
 import { useState } from 'react'
@@ -13,7 +13,7 @@ const TodoItem = ({ todo }) => {
   // get user information from context
 
   const [userInfo] = useOutletContext()
-  const [error, setError] = useState()
+  const [error, setError] = useState("")
 
   // set checked status on checkbox
 
@@ -54,11 +54,13 @@ const TodoItem = ({ todo }) => {
 
   // update todo status 
 
-  const updateTodoStatus = async () => {
-
-    let payload = todo.status == 0 ? { status: 1 } : { status: 0 }
-    console.log('update runs', payload)
-
+  const updateTodoStatus = async (complete) => {
+    let payload
+    if (!complete) {
+      payload = todo.status == 0 ? { status: 1 } : { status: 0 }
+    } else {
+      payload = todo.status != 2 ? { status: 2 } : { status: 0 }
+    }
     try {
       const response = await fetch(
         `${uri}todo/${userInfo._id}/${todo._id}`,
@@ -84,29 +86,106 @@ const TodoItem = ({ todo }) => {
     }
   }
 
+  // set the button states, Start, pause and complete.
+
+  let button;
+
+  if (todo.status == 0) {
+    button = <button title="Start" className="button__play" onClick={() => updateTodoStatus(false)}>
+      <FontAwesomeIcon icon={faPlay} /> <span>Start</span>
+    </button>
+  } else if (todo.status == 1) {
+    button = <button title="Start" className="button__pause" onClick={() => updateTodoStatus(false)}>
+      <FontAwesomeIcon icon={faPause} /> <span>Pause</span>
+    </button>
+  } else {
+    button = <button title="Start" className="button__complete">
+      <FontAwesomeIcon icon={faCheck} /> <span>Complete</span>
+    </button>
+  }
+
+  // set the edit state
+
+  const [edit, setEdit] = useState(false);
+  const [title, setTitle] = useState(todo.title);
+  const [description, setDescription] = useState(todo.description)
+
+
+  //update todo values
+
+  const updateTodo = async (payload) => {
+    let key = Object.keys(payload)[0]
+    if (payload[key] != todo[key]) {
+      try {
+        const response = await fetch(
+          `${uri}todo/${userInfo._id}/${todo._id}`,
+          {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              'auth-token': userInfo.token,
+              Connection: 'keep-alive'
+            },
+            body: JSON.stringify(
+              payload
+            )
+          }
+        )
+        const data = await response.json()
+
+        if (data.message) {
+          setError(data.error)
+        }
+      } catch (err) {
+        setError(err.message)
+      }
+    }
+
+  }
+
+  //stop propagation on click
+
+  const setEditValue = (e) => {
+    e.stopPropagation();
+    setEdit(!edit)
+  }
+
   return (
-    <li className='todoItem glass--soft-dark'>
+    <li className='todoItem glass--soft-dark' onClick={() => setEdit(false)}>
       <button title="Start" className="button__play" htmlFor="complete">
-        <input type="checkbox" name="complete" id="complete" defaultChecked={checked} />
+        <input type="checkbox" name="complete" id="complete" defaultChecked={checked} onChange={() => updateTodoStatus(true)} />
       </button>
       <div className='todoItem__content'>
-        <h3>{todo.title}</h3>
-        <p>{todo.description}</p>
+        {!edit ?
+          <h3>
+            {title}
+          </h3>
+          :
+          <input type="text" name="name" id="name"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            onBlur={() => updateTodo({ title: title })} />}
+
+        {!edit ?
+          <p>{todo.description}</p>
+          :
+          <textarea type="text" name="name" id="name"
+            defaultValue={description}
+            onChange={(e) => setDescription(e.target.value)}
+            onBlur={() => updateTodo({ description: description })} ></textarea>}
       </div>
 
-      {/* add these classes programmatically based in the status
-        button__pause 
-        button__complete
-      */}
-      <button title="Start" className="button__play" onClick={updateTodoStatus}>
-        <FontAwesomeIcon icon={faPlay} /> <span>Start</span>
+      {button}
+      <button className="button--no-text constructive-action button--float-right" title="edit todo" onClick={(e) => setEditValue(e)}>
+        <FontAwesomeIcon icon={faEdit} />
       </button>
       <button className="button--no-text destructive-action button--float-right" title="delete todo" onClick={deleteTodo}>
         <FontAwesomeIcon icon={faTrash} />
       </button>
+
       {error && (<p>error</p>)}
 
-    </li>
+    </li >
   )
 }
 
